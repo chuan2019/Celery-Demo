e996e8dd341e807257e29b6803e2066415d57504
Chuan <47965229+chuan2019@users.noreply.github.com>
2022-01-18 22:00:26 -0800

    Update tasks.py

--- demo-01_docker/tasks.py
+++ demo-01_docker/tasks.py
@@ -1,6 +1,8 @@
+"""tasks.py"""
+# pylint: disable=C0103
 import os
-from celery import Celery
 import time
+from celery import Celery
 
 broker_url  = os.environ.get("CELERY_BROKER_URL",
                              "redis://localhost:6378/0")
@@ -13,8 +15,11 @@ celery_app = Celery(name           = 'tasks',
 
 @celery_app.task
 def add(x, y):
+    '''
+    mimic time consuming task:
+        return sum of the two integers after sleeping for 5 seconds
+    '''
     for i in range(5):
         time.sleep(1)
         print(i)
     return x+y
-
